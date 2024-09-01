a8a794975fc8a7cb04b9c727c5e3a5b5f96869c5
Mmesoma Okenwa <godsfavourokenwa12@gmail.com>
2024-09-01 17:59:16 +0100

    fix product form bug

--- src/lib/actions/product-form-actions.ts
+++ src/lib/actions/product-form-actions.ts
@@ -7,115 +7,138 @@ import { formatIssues, generateFormData } from "../utils"
 
 
 export const createProduct = async (prevState: any, formData: FormData): Promise<ProductFormState> => {
-  const values: FormDataEntries = {
-    ...Object.fromEntries(formData),
-    images: formData.getAll('images')
-  };
+  try {
+    const values: FormDataEntries = {
+      ...Object.fromEntries(formData),
+      images: formData.getAll("images"),
+    };
 
-  const { data, error } = productSchema.safeParse(values)
+    const { data, error } = productSchema.safeParse(values);
 
-  if (error) {
-    return {
-      status: 'error',
-      message: 'Invalid form data',
-      fieldErrors: formatIssues(error.issues)
+    if (error) {
+      return {
+        status: "error",
+        message: "Invalid form data",
+        fieldErrors: formatIssues(error.issues),
+      };
     }
-  }
 
-  const newFormData = generateFormData(data)
+    const newFormData = generateFormData(data);
 
-  const res = await fetch(`${baseUrl}/api/products`, {
-    method: "POST",
-    body: newFormData,
-  });
+    const res = await fetch(`${baseUrl}/api/products`, {
+      method: "POST",
+      body: newFormData,
+    });
 
-  if (!res.ok) {
-    console.log(res.statusText);
-    return {
-      status: 'error',
-      message: 'Failed to create product',
-      fieldErrors: null
+    if (!res.ok) {
+      console.log(res.statusText);
+      return {
+        status: "error",
+        message: "Failed to create product",
+        fieldErrors: null,
+      };
     }
-  }
 
-  return {
-    status: 'success',
-    message: 'Product created successfully',
-    fieldErrors: null
+    return {
+      status: "success",
+      message: "Product created successfully",
+      fieldErrors: null,
+    };
+  } catch (error) {
+    return {
+      status: "error",
+      message: "Failed to create product",
+      fieldErrors: null,
+    }
   }
 }
 
 export const editProduct = async (prevState: any, formData: FormData): Promise<ProductFormState> => {
-  const values: FormDataEntries = {
-    ...Object.fromEntries(formData),
-    images: formData.getAll("images"),
-  }
+  try {
+    const values: FormDataEntries = {
+      ...Object.fromEntries(formData),
+      images: formData.getAll("images"),
+    };
 
-  if (!values.id) {
-    return {
-      status: 'error',
-      message: 'Invalid form data',
-      fieldErrors: null
+    if (!values.id) {
+      return {
+        status: "error",
+        message: "Invalid form data",
+        fieldErrors: null,
+      };
     }
-  }
 
-  const { data, error } = productSchema.safeParse(values)
+    const { data, error } = productSchema.safeParse(values);
 
-  if (error) {
-    return {
-      status: 'error',
-      message: 'Invalid form data',
-      fieldErrors: formatIssues(error.issues)
+    if (error) {
+      return {
+        status: "error",
+        message: "Invalid form data",
+        fieldErrors: formatIssues(error.issues),
+      };
     }
-  }
 
-  const newFormData = generateFormData(data)
+    const newFormData = generateFormData(data);
 
-  const res = await fetch(`${baseUrl}/api/products/${values.id}`, {
-    method: "PATCH",
-    body: newFormData,
-  });
+    const res = await fetch(`${baseUrl}/api/products/${values.id}`, {
+      method: "PATCH",
+      body: newFormData,
+    });
 
-  if (!res.ok) {
-    console.log(res.statusText);
-    return {
-      status: 'error',
-      message: 'Failed to update product',
-      fieldErrors: null
+    if (!res.ok) {
+      console.log(res.statusText);
+      return {
+        status: "error",
+        message: "Failed to update product",
+        fieldErrors: null,
+      };
     }
-  }
 
-  return {
-    status: 'success',
-    message: 'Product updated successfully',
-    fieldErrors: null
+    return {
+      status: "success",
+      message: "Product updated successfully",
+      fieldErrors: null,
+    };
+  } catch (error) {
+    return {
+      status: "error",
+      message: "Failed to update product",
+      fieldErrors: null,
+    }
   }
 }
 
 export const deleteProduct = async (prevState: any, formData: FormData): Promise<Omit<ProductFormState, 'fieldErrors'>> => {
-  const productId = formData.get('id')
+  try {
+    const productId = formData.get("id");
 
-  if (!productId) {
-    return {
-      status: 'error',
-      message: 'Invalid form data',
+    if (!productId) {
+      return {
+        status: "error",
+        message: "Invalid form data",
+      };
     }
-  }
 
-  const res = await fetch(`${baseUrl}/api/products/${productId}`, {
-    method: "DELETE",
-  });
+    const res = await fetch(`${baseUrl}/api/products/${productId}`, {
+      method: "DELETE",
+    });
 
-  if (!res.ok) {
-    console.log(res.statusText);
-    return {
-      status: 'error',
-      message: 'Failed to delete product',
+    if (!res.ok) {
+      console.log(res.statusText);
+      return {
+        status: "error",
+        message: "Failed to delete product",
+      };
     }
-  }
 
-  return {
-    status: 'success',
-    message: 'Product deleted successfully',
+    return {
+      status: "success",
+      message: "Product deleted successfully",
+    };
+  } catch (error) {
+    return {
+      status: "error",
+      message: "Failed to delete product",
+    }
   }
 }
--- src/lib/hooks/useCategories.tsx
+++ src/lib/hooks/useCategories.tsx
@@ -12,23 +12,29 @@ const useCategories = () => {
 
   useEffect(() => { 
     const fetchCategories = async () => {
-      setIsLoading(true)
+      try {
+        setIsLoading(true);
 
-      const res = await fetch('/api/categories', {
-        signal: controller.signal
-      })
+        const res = await fetch("/api/categories", {
+          signal: controller.signal,
+        });
 
-      if (!res.ok) { 
-        setError(res.statusText)
+        if (!res.ok) {
+          setError(res.statusText);
 
-        return setIsLoading(false)
+          return setIsLoading(false);
+        }
+
+        const data = await res.json();
+
+        setData(data);
+      } catch (error) {
+        if (error instanceof Error) {
+          setError(error.message);
+        }
+      } finally {
+        setIsLoading(false);
       }
-
-      const data = await res.json()
-
-      setIsLoading(false)
-
-      setData(data)
     }
 
     fetchCategories()
